e050db04da1c8d61728412eaf4bb7da3c45418ce
Haroon Ahmed <haroon.ahmed25@gmail.com>
2023-05-18 15:28:11 +0100

    Add spec and main attributes:

---
+++ lib/deadfire/spec.rb
@@ -0,0 +1,116 @@
+module Deadfire
+  class Spec
+    # EBNF
+    # stylesheet         = [ CDO | CDC | S | statement ]*;
+    # statement          = ruleset | at-rule;
+    # at-rule            = ATKEYWORD S* any-value* [ block | ';' S* ];
+    # ruleset            = selector [ ',' S* selector ]* S* '{' S* declaration [ ';' S* declaration ]* '}' S*;
+    # selector           = simple-selector [ combinator selector | S+ [ combinator? selector ]? ];
+    # simple-selector    = element-name? [ '#' id-selector ]? [ '.' class-selector ]* [ '[' attrib-selector ']' ]*;
+    # id-selector        = NAME;
+    # class-selector     = '.' NAME;
+    # attrib-selector    = NAME [ [ '=' | INCLUDES | DASHMATCH ] any-value ]?;
+    # combinator         = '+' | '>' | '~' | S+;
+    # declaration        = property ':' S* value;
+    # property           = NAME;
+    # value              = any-value [ ',' S* any-value ]*;
+    # any-value          = IDENT | STRING | NUMBER | PERCENTAGE | DIMENSION | COLOR | URI | FUNCTION any-value* ')' | '(' any-value* ')' | '[' any-value* ']' | '{' any-value* '}' | ';';
+    CSS_AT_RULES = [
+      "@charset",
+      "@import",
+      "@media",
+      "@page",
+      "@font-face",
+      "@keyframes",
+      "@supports",
+      "@namespace",
+      "@counter-style",
+      "@viewport",
+      "@document"
+    ]
+
+    CSS_SELECTORS = [
+      ":root",
+      "::before",
+      "::after",
+      ":hover",
+      ":active",
+      ":focus",
+      ":first-child",
+      ":last-child",
+      ":nth-child(n)",
+      ":nth-last-child(n)",
+      ":only-child",
+      ":only-of-type",
+      ":first-of-type",
+      ":last-of-type",
+      ":nth-of-type(n)",
+      ":nth-last-of-type(n)",
+      ":checked",
+      ":disabled",
+      ":enabled",
+      ":empty",
+      "::first-line",
+      "::first-letter",
+      "::selection",
+      "~",
+      "+",
+      ">",
+      " ",
+      ".class",
+      "#id",
+      "[attribute]",
+      "[attribute=value]",
+      "[attribute~=value]",
+      "[attribute|=value]",
+      "[attribute^=value]",
+      "[attribute$=value]",
+      "[attribute*=value]",
+      ":not(selector)",
+      ":matches(selector)",
+      ":any(selector)",
+      ":has(selector)",
+      "::placeholder"
+    ]
+
+    CSS_POTENTIAL_VALUES_TYPES = [
+      :length_and_size, # e.g. 5px or 5em
+      :color, # e.g. hex codes, RGB and RGBA values, HSL and HSLA values, and named colors - #fff
+      :textual, # strings, keywords, and URLs - bold, url, etc.
+      :enumerated, # e.g. inline, block, list-item, etc.
+      :functional, # e.g.  calc(), attr(), and var() - calc(100% - 10px)
+      :other # everything else like inherit, initial, unset
+
+    COMMON_CSS_PROPERTIES = [
+      "color",
+      "background-color",
+      "background-image",
+      "border",
+      "border-radius",
+      "box-shadow",
+      "font-family",
+      "font-size",
+      "font-weight",
+      "letter-spacing",
+      "line-height",
+      "margin",
+      "padding",
+      "text-align",
+      "text-decoration",
+      "text-transform",
+      "width",
+      "height",
+      "display",
+      "flex",
+      "flex-direction",
+      "justify-content",
+      "align-items",
+      "position",
+      "top",
+      "right",
+      "bottom",
+      "left",
+      "z-index"
+    ]
+  end
+end
