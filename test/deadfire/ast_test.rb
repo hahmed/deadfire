f437ba1fa6632ac6ead0fa6a2c3929aa310ca71a
Haroon Ahmed <haroon.ahmed25@gmail.com>
2024-06-05 23:51:05 +0100

    Add basic test for AST

---
+++ test/deadfire/ast_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class AstTest < Minitest::Test
+  def test_ast_prints_correctly
+    css = <<~CSS
+      /* comment */
+      .test_css_1 {
+        padding: 1rem;
+      }
+
+      p { size: 1rem; }
+    CSS
+    parser = Deadfire::ParserEngine.new(css)
+    out, err = capture_io do
+      parser.print_ast
+    end
+
+    assert_includes out, "CommentNode"
+  end
+end
