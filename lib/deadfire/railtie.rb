f06b836e1e5199863e3af5744848be97689a1b01
Haroon Ahmed <haroon.ahmed25@gmail.com>
2024-08-06 21:29:32 +0100

    Update railties

--- lib/deadfire/railtie.rb
+++ lib/deadfire/railtie.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 require "rails"
+require "propshaft"
 
 module Deadfire
   class Railties < ::Rails::Railtie
-    unless defined?(Propshaft)
-      require "propshaft"
-    end
-
     class DeadfireCompiler < Propshaft::Compiler
       def compile(logical_path, input)
+        # NOTE: this should change the file...
         Deadfire.parse(input, filename: logical_path)
       end
     end
   
     config.assets.compilers << ["text/css", DeadfireCompiler]
-    
+
+    initializer "excluded paths" do
+      config.assets.excluded_paths << [Rails.root.join("app", "assets", "vendor")]
+    end
+
     config.after_initialize do
+      # config.assets.paths -= [Rails.root.join("app", "assets", "stylesheets", "vendor")]
       config.deadfire = ActiveSupport::OrderedOptions.new
       config.deadfire.root_path = Rails.root.join("app", "assets", "stylesheets")
 
