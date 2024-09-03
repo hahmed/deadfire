2c1fe4f70539ff7746789d41a0acb123d927a359
Haroon Ahmed <haroon.ahmed25@gmail.com>
2024-09-03 21:29:45 +0100

    add the correct root file

--- lib/deadfire/railtie.rb
+++ lib/deadfire/railtie.rb
@@ -4,23 +4,9 @@ require "propshaft"
 
 module Deadfire
   class Railties < ::Rails::Railtie
-    class DeadfireCompiler < Propshaft::Compiler
-      def compile(logical_path, input)
-        # NOTE: this should change the file...
-        Deadfire.parse(input, filename: logical_path)
-      end
-    end
-  
-    config.assets.compilers << ["text/css", DeadfireCompiler]
-
-    initializer "excluded paths" do
-      config.assets.excluded_paths << [Rails.root.join("app", "assets", "vendor")]
-    end
-
     config.after_initialize do
-      # config.assets.paths -= [Rails.root.join("app", "assets", "stylesheets", "vendor")]
       config.deadfire = ActiveSupport::OrderedOptions.new
-      config.deadfire.root_path = Rails.root.join("app", "assets", "stylesheets")
+      config.deadfire.root_path = Rails.root.join("app", "assets")
 
       Deadfire.configure do |deadfire_config|
         deadfire_config.root_path = config.deadfire.root_path
@@ -28,5 +14,13 @@ module Deadfire
         deadfire_config.logger = config.assets.logger || Rails.logger
       end
     end
+
+    class DeadfireCompiler < ::Propshaft::Compiler
+      def compile(logical_path, input)
+        Deadfire.parse(input, filename: logical_path)
+      end
+    end
+  
+    config.assets.compilers << ["text/css", DeadfireCompiler]
   end
 end
